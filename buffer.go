d1bb25bb0f37bb015407f70e27b0e101c01481a6
yisroel <yisroelshulman16@gmail.com>
2025-01-13 23:00:58 -0500

    added comments

--- buffer.go
+++ buffer.go
@@ -1,39 +1,42 @@
 package main
 
 import (
-	"fmt"
 	"sync"
-	"time"
 )
 
+// a buffer for the BufferedLimiter to keep track of the requests waiting for approval.
 type buffer struct {
 	mu       *sync.Mutex
-	buffer   []*permissionStatus
 	capacity int
 	size     int
 	insertAt int
 	removeAt int
+	buffer   []*permissionStatus
 }
 
+// returns a new buffer
+//
+// no need to check if capacity is 0 since this is for internal use only.
 func newBuffer(capacity int) *buffer {
-	b := &buffer{
+	return &buffer{
 		mu:       &sync.Mutex{},
-		buffer:   make([]*permissionStatus, capacity),
 		capacity: capacity,
 		size:     0,
 		insertAt: 0,
 		removeAt: 0,
+		buffer:   make([]*permissionStatus, capacity),
 	}
-	return b
 }
 
+// add the request to the buffer
+// returns true if the request was added and false if the buffer is full
 func (b *buffer) add(access *permissionStatus) bool {
 	b.mu.Lock()
 	defer b.mu.Unlock()
 	if b.size == b.capacity {
 		return false
 	}
-	if b.insertAt == b.removeAt && b.size > 0 {
+	if b.insertAt == b.removeAt && b.size > 0 { // some requests timed out so cleaning is necessary
 		b.cleanBuffer()
 	}
 	b.buffer[b.insertAt] = access
@@ -42,13 +45,13 @@ func (b *buffer) add(access *permissionStatus) bool {
 	return true
 }
 
+// removes the requests that timed out preserving the order of the requests
 func (b *buffer) cleanBuffer() {
-	fmt.Printf(">>>>> cleaning buffer time: %v\n", time.Now())
 	buf := make([]*permissionStatus, b.capacity)
 	pos := b.removeAt
 	insert := 0
 	for i := 0; i < b.capacity; i++ {
-		if b.buffer[pos] != nil && !b.buffer[pos].timedOut {
+		if b.buffer[pos] != nil && !b.buffer[pos].timedOut { // check that the request hasn't timed out
 			buf[insert] = b.buffer[pos]
 			insert++
 		}
@@ -59,13 +62,16 @@ func (b *buffer) cleanBuffer() {
 	b.removeAt = 0
 }
 
+// remove signals to the next requester that access was granted if there is one waiting
+//
+// returns true if a requester is waiting false otherwise
 func (b *buffer) remove() bool {
 	b.mu.Lock()
 	defer b.mu.Unlock()
 	if b.size == 0 {
 		return false
 	}
-	for b.buffer[b.removeAt] == nil || b.buffer[b.removeAt].timedOut {
+	for b.buffer[b.removeAt] == nil || b.buffer[b.removeAt].timedOut { // advance to the next requester
 		b.buffer[b.removeAt] = nil
 		b.removeAt = incrementIndex(b.removeAt, b.capacity)
 	}
@@ -76,12 +82,9 @@ func (b *buffer) remove() bool {
 	return true
 }
 
+// the requester signals to the buffer that the request timed out
 func (b *buffer) timedOutSignal() {
 	b.mu.Lock()
 	defer b.mu.Unlock()
 	b.size--
-}
-
-func incrementIndex(index, capacity int) int {
-	return (index + 1) % capacity
 }
